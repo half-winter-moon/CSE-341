9f8871499043eb49715c93d5d82545d3554dbb92
half-winter-moon <han16064@byui.edu>
2022-04-20 09:00:24 -0600

    in class activity

---
+++ lesson-01-class-activity/controllers/index.js
@@ -0,0 +1,9 @@
+displayQuote = (res, req) => {
+    const data = 
+    'Do or do not, there is no try';
+    res.status(200).send(data);
+};
+
+module.exports = {
+    displayQuote,
+};
---
+++ lesson-01-class-activity/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const routes = require('./routes');
+const app = express();
+const port = 300;
+
+app.use('/', require('./routes'));
+
+
+app.listen(port, () => {
+    console.log('Example app listening on port 3000');
+});
